4de9b1d368ef2da5a4bf0b70ffff7c6081a848be
jiebei <jiebeiya668@163.com>
2025-11-14 17:24:36 +0800

    gradio chatbot改用了blocks方式，并且增加成为两个tab页。目前实例examples还没实现

--- demo01/langchain_agent.py
+++ demo01/langchain_agent.py
@@ -1,3 +1,4 @@
+import time
 
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
@@ -18,56 +19,74 @@ model = ChatOpenAI(
 )
 agent = create_agent(model, tools=[rag, construct_kg])
 
-# while True:
-#     user_input = input()
-#     # response = agent.invoke({"message": [{"role": "user", "content": "hello"}]})
-#     # NOTE: 没有cnotallow不能跑
-#     result1 = agent.invoke({"messages": [{"role": "user", "content": user_input}]},
-#                                cnotallow={"configurable": {"thread_id": "user_1"}})
-#     print(result1)
 
-
+# 第一个参数和第二个参数一个是历史，一个是当前信息，貌似和gr.Textbox().submit第二个参数顺序有关
 def chat_with_agent(message, chat_history):
-    messages = [{"role": "user", "content": message}]
-    result = agent.invoke({"messages": messages}, cnotallow={"configurable": {"thread_id": "user_1"}})
+    format_msg = {"role": "user", "content": message}
+    chat_history.append(format_msg)
+    # messages = [{"role": "user", "content": message}]
+    result = agent.invoke({"messages": chat_history}, cnotallow={"configurable": {"thread_id": "user_1"}})
 
     # 提取Agent的回复
     if hasattr(result, 'get') and 'output' in result:
         response = result['output']
     else:
-        response = str(result)
+        response = {"role": "assistant", "content": str(result)}
 
         # 更新聊天历史
-        chat_history.append((message, response))
+        chat_history.append(response)
 
-        return response
-
-def yes_man(message, history):
-    if message.endswith("?"):
-        return "Yes"
-    else:
-        return "Ask me anything!"
+        return "", chat_history
 
 
-def run_webui():
-    chat_app = gr.ChatInterface(
-        chat_with_agent,
-        chatbot=gr.Chatbot(height=400),
-        textbox=gr.Textbox(placeholder="请输入你的问题", container=False, scale=7),
-        title="YWAgent",
-        description="你可以问关于运维的问题",
-        theme="default",
-        examples=["根据文档库总结西游记主要内容",
-                  "请根据所给文字来构建知识图谱，文字为：李明是张华的表哥，而王芳则是他们共同的大学同学。"],
-        cache_examples=False,
-        submit_btn="发送",
-        stop_btn="停止",
+
+# 流式产生内容
+def create_stream_output(history: list):
+    response = history[-1]["content"]
+    history.pop()
+    template = {"role": "assistant", "content": ""}
+    history.append(template)
+    for character in response:
+        history[-1]["content"] += character
+        time.sleep(0.05)
+
+        yield history
+
+
+def main():
+    with gr.Blocks() as demo:
+        with gr.Tab("待扩展"):
+            gr.Textbox(label="待扩展")
+        with gr.Tab("YWAent"):
+            gr.Markdown("我是运维专家，你可以问我相关的问题！")
+
+            chatbot = gr.Chatbot(elem_id="chatbot", bubble_full_width=False, type="messages")
+
+            # chat_input = gr.MultimodalTextbox(
+            #     interactive=True,
+            #     file_count="multiple",
+            #     placeholder="请输入或者上传文件",
+            #     show_label=False,
+            #     sources=["microphone", "upload"],
+            # )
+
+            chat_input = gr.Textbox("请和我对话。。。")
+
+            chat_msg = chat_input.submit(
+                chat_with_agent, [chat_input, chatbot], [chat_input, chatbot]
+            )
+            bot_msg = chat_msg.then(create_stream_output, chatbot, chatbot, api_name="bot_response")
+            # bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
+            bot_msg.then(lambda: gr.Textbox(interactive=True), None, [chat_input])
+
+
+    demo.launch(
+        server_name="0.0.0.0"
+        , server_port=int(Config.get_instance().get_with_nested_params("server", "ui_port"))
+        , share=Config.get_instance().get_with_nested_params("server", "ui_share")
+        , max_threads=10
     )
 
-    chat_app.launch(server_name="0.0.0.0"
-                    , server_port=int(Config.get_instance().get_with_nested_params("server", "ui_port"))
-                    , share=Config.get_instance().get_with_nested_params("server", "ui_share")
-                    , max_threads=10)
 
 if __name__ == '__main__':
-    run_webui()
+    main()
